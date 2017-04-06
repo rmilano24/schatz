5365070645a4c088cae9e88f909b4eb8257ad356
Ryan Milano <rpm@Ryans-MBP.attlocal.net>
2017-04-06 11:37:20 -0700

    add project images and rework services css

--- wp-content/themes/shatz/templates/portfolio/template-top.php
+++ wp-content/themes/shatz/templates/portfolio/template-top.php
@@ -4,9 +4,9 @@
 	$row_class = '';
 	$mvb_content = get_post_meta(get_the_ID(), '_bshaper_artist_content', true );
 	$mvb_enable = get_post_meta( get_the_ID(), '_bshaper_activate_metro_builder', true );
-	
+
 	$page_content = get_the_content();
-	
+
 	if (!empty($mvb_content) && strcmp($mvb_enable, '1') === 0):
 	?>
 		<div class="row-portfolio-template-mvb-content">
